3fa3f41a6a7e3c69206b2f5aed9b68970c25d40c
Antonino Simone Di Stefano <simon.distefano@gmx.de>
2018-02-11 10:02:53 +0100

    New experiment file 8. Fold size is estimated by absolute number of samples to evaluate. Besides, best combination so far, is printed on the console

--- matlab/echolex/prototyping/lexsub/experiments/bigram_substitution_test_6.m
+++ matlab/echolex/prototyping/lexsub/experiments/bigram_substitution_test_6.m
@@ -38,9 +38,7 @@ results_fields = ...
 results_fields_acc = {'mean_acc_orig_nb', 'mean_acc_sub_n_nb', 'mean_acc_orig_svm', 'mean_acc_sub_n_svm'};
 param_fields = 1:6;
 prefix = 'results_test_run';
-%dtstamp = char(datetime);
-%dtstamp = strrep(dtstamp, ':', '');
-%results_dir_path = fullfile(echolex_dumps, prefix, dtstamp);
+
 results_dir_path = fullfile(echolex_dumps, prefix);  
 mkdir(results_dir_path);
 
@@ -70,7 +68,6 @@ params_ngrams(params_ngrams(:,3) == 0,:) = [];
 params_ngrams = [params_ngrams; b0];
 
 load(fullfile(echolex_dumps, 'params_bigrams_all.mat'));
-% params_ngrams = [0.250000000000000 1 0.300000000000000 0];
 
 %% Iterate through datasets
 
